ff9a1e0884b4ff115dbab731f472038ed2ad4cdd
mins00kwon <mskwon0918@gamil.com>
2025-01-18 13:14:45 +0900

    메세지 전송에 변수 기능 추가

--- src/test_cases/message/message_management/send_message.sql
+++ src/test_cases/message/message_management/send_message.sql
@@ -1,20 +1,24 @@
+SET @send_user =1;
+SET	@receive_user=5;
+SET @message_test="Hello World!";
+
 INSERT INTO message (message_contents, from_user_id, to_user_id)
 SELECT 
-    'Hello, this is a message!' AS message_contents,
-    1 AS from_user_id,
-    5 AS to_user_id
+    (SELECT @message_test) AS message_contents,
+    (SELECT @send_user) AS from_user_id,
+    (SELECT @receive_user) AS to_user_id
 WHERE 
     -- 수신자가 비공개 계정이 아닌 경우
-    (SELECT user_status FROM user WHERE user_id = 5) = 'PUBLIC'
+    (SELECT user_status FROM user WHERE user_id = (SELECT @receive_user)) = 'PUBLIC'
     -- 수신자가 비공개 계정이더라도 발신자를 팔로우하고 있는 경우
     OR EXISTS (
         SELECT 1 -- 수정 x
         FROM follow 
-        WHERE follower_id = 5 AND followed_id = 1 AND is_acceptance = TRUE
+        WHERE follower_id = @receive_user AND followed_id = (SELECT @send_user) AND is_acceptance = TRUE
     )
     -- 수신자가 발신자를 차단하지 않은 경우
     AND NOT EXISTS (
         SELECT 1 -- 수정 x
         FROM ban 
-        WHERE user_ban_id = 5 AND user_banned_id = 1
+        WHERE user_ban_id = @receive_user AND user_banned_id = (SELECT @send_user)
     );
