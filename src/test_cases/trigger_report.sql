f7bf67ee63a20333dacabaff96fcf36d4355a0c4
oni128 <lyw283828@gmail.com>
2025-01-18 18:00:57 +0900

    신고 트리거 테스트 데이터 추가

--- src/test_cases/trigger_report.sql
+++ src/test_cases/trigger_report.sql
@@ -41,3 +41,17 @@ UPDATE report
 SET report_approved = 'YES'
 WHERE reported_user_id = '2';
 SELECT * FROM user WHERE user_id = 2;
+
+-- -------------------
+INSERT INTO report (created_at, report_contents, report_approved, report_type, reported_user_id, report_user_id, comment_id)
+VALUES
+    (CURRENT_TIMESTAMP, '욕설이 포함된 댓글입니다.', 'NO', 'COMMENT', 2, 5, 5);
+
+
+iNSERT INTO report (created_at, report_contents, report_approved, report_type, reported_user_id, report_user_id, comment_id)
+VALUES
+    (CURRENT_TIMESTAMP, '욕설이 포함된 댓글입니다.', 'NO', 'COMMENT', 2, 5, 4);
+
+iNSERT INTO report (created_at, report_contents, report_approved, report_type, reported_user_id, report_user_id, comment_id)
+VALUES
+    (CURRENT_TIMESTAMP, '욕설이 포함된 댓글입니다.', 'NO', 'COMMENT', 2, 5, 3);
