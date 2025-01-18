8d40522d3178213075c01ea6543c9b3f74573acc
mins00kwon <mskwon0918@gamil.com>
2025-01-18 13:09:05 +0900

    메세지 목록 조회 기능에 변수 기능 추가

--- src/test_cases/message/message_management/view_message_list.sql.sql
+++ src/test_cases/message/message_management/view_message_list.sql.sql
@@ -1,14 +1,16 @@
+SET @view_message_list_user_id=5;
+
 SELECT 
     CASE 
-        WHEN from_user_id = 1 THEN to_user_id
+        WHEN from_user_id = (SELECT @view_message_list_user_id) THEN to_user_id
         ELSE from_user_id
     END AS other_user_id, -- 대화를 나눈 상대방 ID
     MAX(message_send_at) AS last_message_at   -- 가장 최근 메시지 시간
 FROM message
 WHERE 
-    from_user_id = 1 OR to_user_id = 1 -- 내가 발신자이거나 수신자인 경우
+    from_user_id = (SELECT @view_message_list_user_id) OR to_user_id = (SELECT @view_message_list_user_id) -- 내가 발신자이거나 수신자인 경우
 GROUP BY 
     CASE 
-        WHEN from_user_id = 1 THEN to_user_id
+        WHEN from_user_id = (SELECT @view_message_list_user_id) THEN to_user_id
         ELSE from_user_id
     END;
