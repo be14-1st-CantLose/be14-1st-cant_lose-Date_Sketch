d13c699ea1f084a196243c20c2769f4c400a034a
oni128 <124542644+oni128@users.noreply.github.com>
2025-01-18 15:59:06 +0900

    댓글관리 변경사항으로 인한 삭제

--- src/test_cases/manage_comments/register_course_comment.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO comment (comment_contents, created_at, updated_at, is_deleted, likes, user_id, comment_type, course_id, place_id, recomment_id)
-VALUES 
-    -- 코스에 대한 일반 댓글
-    ('정말 멋진 코스입니다!', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE, 10, 1, 'COURSE', 1, NULL, NULL),
-    ('정말 멋진 코스입니다.......', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE, 10, 1, 'COURSE', 1, NULL, NULL),
-    ('이 코스는 관리가 필요해 보입니다.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE, 5, 2, 'COURSE', 1, NULL, NULL);
