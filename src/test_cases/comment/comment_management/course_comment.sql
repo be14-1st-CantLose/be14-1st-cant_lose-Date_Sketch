bc7dde8c7dad84e188342df273fffbd8a058a285
mins00kwon <mskwon0918@gamil.com>
2025-01-18 17:12:00 +0900

    코스 댓글 오류 수정

--- src/test_cases/comment/comment_management/course_comment.sql
+++ src/test_cases/comment/comment_management/course_comment.sql
@@ -1,16 +1,16 @@
 SET	@var_comment_contents="코스에 달리는 첫 번째 댓글입니다."; -- 댓글 내용
 SET @var_user_id=1; -- 댓글 다는 유저 아이디 
 SET @var_comment_type="COURSE"; -- 코스 댓글
-SET @var_place_id=1; -- 댓글이 달리는 장소 게시물 id
+SET @var_course_id=1; -- 댓글이 달리는 코스 게시물 id
 INSERT INTO comment (
     comment_contents, 
     user_id, 
     comment_type, 
-    place_id
+    course_id
 ) 
 VALUES (
     @var_comment_contents, 
     @var_user_id, 
     @var_comment_type, 
-    @var_place_id
+    @var_course_id
 );
